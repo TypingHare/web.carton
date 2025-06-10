7ff5a8cd4e08b57bf41ee88fb0c0c5893fa952f1
James Chen <jameschan312.cn@gmail.com>
2025-06-10 10:33:37 -0400

    Implemented NotFoundCommand

--- src/main/kotlin/command/NotFoundCommand.kt
+++ src/main/kotlin/command/NotFoundCommand.kt
@@ -1,12 +1,21 @@
 package burrow.carton.web.command
 
-import burrow.carton.core.command.NotFoundCommand as OriginalNotFoundCommand
+import burrow.carton.hoard.RecordNotFoundException
 import burrow.carton.web.Web
 import picocli.CommandLine
+import burrow.carton.core.command.NotFoundCommand as OriginalNotFoundCommand
 
+@CommandLine.Command(name = "<not-found>")
 class NotFoundCommand : OriginalNotFoundCommand() {
     override fun call(): Int {
-        use(Web::class).getAllWebRecords()
+        val first = commandList.first()
+
+        try {
+            val webRecord = use(Web::class).getWebRecordByName(first)
+            dispatch(OpenCommand::class, webRecord.name)
+        } catch (_: RecordNotFoundException) {
+            super.call()
+        }
 
         return CommandLine.ExitCode.OK
     }
